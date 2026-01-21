6277a0f5f06ee2da3a94a1123f31d186750ce36d
Bryan Reyes <breyes2022@fau.edu>
2026-01-20 23:45:03 -0500

    new version of redirects

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-    base: '/bill-e/', // Critical for assets to load correctly
-    plugins: [react()],
-  })
+  base: '/bill-e/',
+  plugins: [react()],
+})
