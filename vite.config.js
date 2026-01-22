18f1f7b517172a2dc5f3806a30064f7a956a8afa
Bryan Reyes <breyes2022@fau.edu>
2026-01-21 23:24:35 -0500

    fix routing in index

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/bill-e/',
-  plugins: [react()],
-})
+    base: '/bill-e/', 
+    plugins: [react()],
+    build: {
+      outDir: 'dist',
+    }
+  })
